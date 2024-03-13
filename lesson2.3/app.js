3ca370ae1e36cfd84137f686a0d76bebcc83ffa2
Yana Shestakova <yana.shestakova.target@gmail.com>
2024-03-13 20:28:10 +0100

    move files

---
+++ lesson2.3/app.js
@@ -0,0 +1,17 @@
+const titleProject = 'Oценка проекта'
+console.log(typeof titleProject)
+
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+console.log(typeof screensValue)
+
+const screenPrice = 21;
+console.log(typeof screenPrice)
+
+const percentage = 40
+console.log(typeof percentage)
+
+const fullPrice = 500
+console.log(typeof fullPrice)
+
+const responsive = true
+console.log(typeof responsive)
